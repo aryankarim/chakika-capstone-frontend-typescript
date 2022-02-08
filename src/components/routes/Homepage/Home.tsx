7db8e333e9bf40f529377450c38f5df284260f23
aryanmajeed <aryanmajid97@gmail.com>
2022-02-09 02:50:44 +0300

    style(About.tsx): add about content

--- src/components/routes/Homepage/Home.tsx
+++ src/components/routes/Homepage/Home.tsx
@@ -2,19 +2,20 @@ import { ReactElement, useRef } from "react";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import Header from "./Header";
 import HowWeWork from "./HowWeWork";
+import About from "./About";
 
 export default function Home(): ReactElement {
   const ref = useRef<any>();
   return (
     <Parallax pages={3} ref={ref} className="bg-darkGreen">
-      <ParallaxLayer offset={0} className="flex justify-center bg-red-400">
+      <ParallaxLayer offset={0} className="flex justify-center">
         <Header />
       </ParallaxLayer>
-      <ParallaxLayer offset={1} speed={0.3} className="flex justify-center">
+      <ParallaxLayer offset={1} speed={0.3} className="flex justify-center ">
         <HowWeWork />
       </ParallaxLayer>
       <ParallaxLayer offset={2} className="flex justify-center">
-        <HowWeWork />
+        <About />
       </ParallaxLayer>
     </Parallax>
   );
