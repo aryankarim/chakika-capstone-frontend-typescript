4dbe1936582f227d156058f588b58211a40fe5fc
aryanmajeed <aryanmajid97@gmail.com>
2022-02-09 03:44:43 +0300

    style(HowWeWork.tsx): use REM for text size

--- src/components/routes/Homepage/About.tsx
+++ src/components/routes/Homepage/About.tsx
@@ -5,12 +5,12 @@ type Props = {};
 export default function About({}: Props) {
   return (
     <div className="grid grid-cols-2 bg-lightGreen items-center text-darkGreen">
-      <div className="col-span-2 lg:col-span-1 justify-self-center w-4/6">
+      <div className="col-span-2 lg:col-span-1 justify-self-center self-end lg:self-center w-4/6">
         <img src={aboutimage} alt="chakika image" />
       </div>
       <div className="flex flex-col col-span-2 lg:col-span-1 items-center lg:items-start">
-        <h2 className="text-xl font-bold my-6">What is Chakika?</h2>
-        <p className="mx-10 lg:ml-0 lg:mr-20 text-justify">
+        <h2 className="text-lg md:text-3xl font-bold my-6">What is Chakika?</h2>
+        <p className="mx-10 lg:ml-0 lg:mr-20 text-justify text-sm md:text-md">
           Chakika will be a web application for the local community in Kurdistan
           and possibly in Iraq in the long term. Chakika will most likely be the
           first online service working on remote cars by sending dedicated
