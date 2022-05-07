6bdd2253ef1f11ba310e1e85aca786f2eb5a56d7
aryanmajeed <aryanmajid97@gmail.com>
2022-05-07 23:57:34 +0300

    style(Navbar): make navbar responsive

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import { IoIosMenu } from "react-icons/io";
+import { useState } from "react";
 
 const navItems = [
   { pathName: "Home", href: "/" },
@@ -10,11 +12,30 @@ const navItems = [
 ];
 
 export default function Navbar() {
+  const [isClosed, setIsClosed] = useState(true);
+
+  const openMenu = () => {
+    setIsClosed((prev) => !prev);
+  };
+
   return (
-    <nav className="container mx-auto flex items-center gap-4 h-10">
-      {navItems.map(({ pathName, href }) => {
-        return <Link to={href}>{pathName}</Link>;
-      })}
-    </nav>
+    <>
+      <nav
+        className={`md:container md:mx-auto flex items-start md:items-center justify-between ${
+          !isClosed ? "h-screen" : "h-10"
+        } md:h-10`}
+      >
+        <IoIosMenu className="md:hidden w-6 h-6 m-2" onClick={openMenu} />
+        <div
+          className={`${
+            isClosed ? "hidden" : "flex pt-10"
+          } md:flex items-center gap-4 bg-white flex-col md:flex-row w-full `}
+        >
+          {navItems.map(({ pathName, href }) => {
+            return <Link to={href}>{pathName}</Link>;
+          })}
+        </div>
+      </nav>
+    </>
   );
 }
