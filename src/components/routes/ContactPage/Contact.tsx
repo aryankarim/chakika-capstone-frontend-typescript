4bc3f1a4763e8f6bf37f7c54e5a7b51afa225a5f
AryanKarim <aryanmajid97@gmail.com>
2022-05-23 01:06:12 +0300

    refactor(Contact): change button to mui button

--- src/components/routes/ContactPage/Contact.tsx
+++ src/components/routes/ContactPage/Contact.tsx
@@ -1,5 +1,4 @@
-import { TextField } from "@mui/material";
-import { buttonStyle } from "../../../utils/constants";
+import { Button, TextField } from "@mui/material";
 
 export default function Contact() {
   return (
@@ -29,7 +28,14 @@ export default function Contact() {
             fullWidth
           />
         </div>
-        <button className={buttonStyle}>Send</button>
+        <Button
+          variant="contained"
+          color="primary"
+          fullWidth
+          sx={{ height: 50 }}
+        >
+          Contained
+        </Button>
       </div>
     </div>
   );
--- src/utils/constants.ts
+++
@@ -1,4 +0,0 @@
-// ClassName Styles
-
-export const buttonStyle =
-  "w-full h-11 px-6 text-lg border rounded bg-chakikaDarkGreen text-white";
